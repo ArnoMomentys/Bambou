ddef11af6b43add3f78c2d6986b7d30bdaa92505
Fabien Naëssens <fnaessens@esensconsulting.com>
2014-10-22 00:47:52 +0200

    test commit

--- _www/app/models/viewEventsEventGuests.php
+++ _www/app/models/viewEventsEventGuests.php
@@ -5,14 +5,14 @@
  */
 class viewEventsEventGuests extends DB\SQL\Mapper
 {
-
     /**
      *
      */
     public function __construct(DB\SQL $db)
     {
 
-    	parent::__construct($db, '_events_eventguests');
+    	parent::__construct($db, '_events_eventguests_nohosts');
+    	$this->db = $db;
     }
 
     /**
@@ -21,12 +21,33 @@ class viewEventsEventGuests extends DB\SQL\Mapper
     public function getEventGuestsPaginated($filters, $options)
     {
 		$page = \Pagination::findCurrentPage();
-		return $this->paginateToArray($page-1, 30, $filters, $options);
+		$result = $this->paginateToArray($page-1, 30, $filters, $options);
+		foreach($result['subset'] as $key=>$guest) {
+			$result['subset'][$key]['hostname'] = $this->getHostsList($guest['guestid'], $filters);
+		}
+		return $result;
     }
 
     /**
      *
      */
+    public function getHostsList($guestId, $filters) 
+    {
+    	$hostName = "";
+    	$sql = "select hostname from _events_eventguests where guestid=? and ".$filters[0];
+    	$params = array(1 => $guestId, 2 => $filters[1]);
+    	//var_dump($params);die;
+    	$results = $this->db->exec("select hostname from _events_eventguests where guestid=? and ".$filters[0],$params);
+    	foreach($results as $host){
+    		$hostName .= " ".$host['hostname'];
+    	}
+    	return $hostName;	 
+    }
+
+    
+    
+    
+    
     public function getGuestsByHost($hostid, $eventid)
     {
 		$filters = array("hostid=? AND eid=?", $hostid, $eventid);
