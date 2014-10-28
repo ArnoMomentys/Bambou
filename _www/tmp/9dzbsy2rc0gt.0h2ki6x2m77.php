3ca419c47da9d8bc94113457ab1cf9af8f4a6c5c
Guillaume Mocquet <gmocquet@esensconsulting.com>
2014-10-28 21:34:33 +0100

    Delete old files

--- _www/tmp/9dzbsy2rc0gt.0h2ki6x2m77.php
+++
@@ -1,8 +0,0 @@
-<?php echo $this->render('header.htm',$this->mime,get_defined_vars()); ?>
-	<?php echo $this->render($view,$this->mime,get_defined_vars()); ?>
-	<?php if (!preg_match('/\/login\/u/i',$PATTERN)): ?>
-	<div class="col-xs-12 col-sm-12 hotline texalicen well">
-		<?php echo $hotline[$LANGUAGE]; ?> <a href="maito:bambou@momentys.fr"><?php echo $mail[$LANGUAGE]; ?></a>
-	</div>
-	<?php endif; ?>
-<?php echo $this->render('footer.htm',$this->mime,get_defined_vars()); ?>
--- _www/tmp/9dzbsy2rc0gt.0hjcfuxeq2v.php
+++
@@ -1,3 +0,0 @@
-<div class="alert alert-info pull-left widful">
-    <i class="glyphicon glyphicon-info-sign"></i>&nbsp;&nbsp;&nbsp;<?php echo html_entity_decode($SESSION['msg']); ?>
-</div>
